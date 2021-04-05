e1c5cad667ce8a0274f3877dcd59c781500462fc
Nicolas Mossmann <nicolas.mossmann@ymail.com>
2021-04-05 18:57:01 -0300

    iOS fixes

--- App/Components/DetailsWeather/DetailsWeather.style.js
+++ App/Components/DetailsWeather/DetailsWeather.style.js
@@ -3,59 +3,59 @@ import { StyleSheet, StatusBar } from 'react-native'
 import { Fonts, Colors } from 'App/Theme'
 
 export default StyleSheet.create({
-  wrapper: {
-    paddingTop: StatusBar.currentHeight + 15,
-    flex: 1,
-  },
-  titleWrapper: {
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  titleText: {
-    ...Fonts.QuicksandMedium,
-    color: Colors.white,
-    textTransform: 'uppercase',
-    fontSize: 16,
-    lineHeight: 18,
-    paddingRight: 15,
-  },
-  line: {
-    flex: 1,
-    height: 1,
-    backgroundColor: Colors.white,
-    opacity: 0.2
-  },
-  detailsWrapper: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    marginTop: 10
-  },
-  detailWrapper: {
-    flexBasis: '30.64%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 15,
-    marginVertical: 5,
-    marginHorizontal: 5,
-    backgroundColor: 'rgba(255, 255, 255, 0.1)',
-    borderRadius: 5
-  },
-  imageWrapper: {
-    height: 32,
-    width: 32,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 5
-  },
   detailTitleText: {
     ...Fonts.QuicksandMedium,
     color: Colors.white,
     fontSize: 14,
-    opacity: 0.8
+    opacity: 0.8,
   },
   detailValueText: {
     ...Fonts.QuicksandMedium,
     color: Colors.white,
-    fontSize: 16
+    fontSize: 16,
+  },
+  detailWrapper: {
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    borderRadius: 5,
+    flexBasis: '30%',
+    justifyContent: 'center',
+    marginHorizontal: 5,
+    marginVertical: 5,
+    paddingVertical: 15,
+  },
+  detailsWrapper: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 10,
+  },
+  imageWrapper: {
+    alignItems: 'center',
+    height: 32,
+    justifyContent: 'center',
+    marginBottom: 5,
+    width: 32,
+  },
+  line: {
+    backgroundColor: Colors.white,
+    flex: 1,
+    height: 1,
+    opacity: 0.2,
+  },
+  titleText: {
+    ...Fonts.QuicksandMedium,
+    color: Colors.white,
+    fontSize: 16,
+    lineHeight: 18,
+    paddingRight: 15,
+    textTransform: 'uppercase',
+  },
+  titleWrapper: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  wrapper: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight + 15,
   },
 })
---
+++ ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Clima
+//
+//  Created by Diego Mossmann on 05/04/21.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+import Foundation
